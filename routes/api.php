4522b95a3563d4cca3a778e0b752806512dae810
AnwarMulyaIbrahim <anwaribrahima7x@gmail.com>
2025-08-11 04:01:30 +0700

    edit api

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CallbackController;
 
 // route callback
-Route::post('callback', CallbackController::class);
+Route::post('/callback', CallbackController::class);
